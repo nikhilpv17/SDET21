549799b5975038960e4e589483b7eecdfa306102
nikhilpv17 <nikhilpv1729@gmail.com>
2021-09-04 15:17:21 +0530

    jhjhjh

---
+++ src/test/java/com/vtiger/comcast/leadtest/Sim1.java
@@ -0,0 +1,10 @@
+package com.vtiger.comcast.leadtest;
+
+public class Sim1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/vtiger/comcast/leadtest/Sim2.java
@@ -0,0 +1,12 @@
+package com.vtiger.comcast.leadtest;
+
+import org.testng.annotations.Test;
+
+public class Sim2 {
+	
+	@Test
+	public void reun33() {
+		System.out.println("this is also from xml");
+	}
+
+}
